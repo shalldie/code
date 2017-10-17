797b0f52f4c6e36d37316cd4c322aa257ae65457
谢爽 <394229785@qq.com>
2017-10-17 08:38:45 +0800

    init

---
+++ typescript&node/src/app.ts
@@ -0,0 +1,12 @@
+import Tool from './lib/Tool';
+
+
+(async () => {
+    let tool = new Tool();
+    for (let i = 0; i < 10; i++) {
+        console.log(new Date().getSeconds());
+        await tool.sleep(1000);
+    }
+})();
+
+const hello = 'world';
